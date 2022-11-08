da39d2c8ca2a276e1d201dfeec29be9b5f853fe3
unknown <uwrmaritha250@gmail.com>
2022-11-08 20:49:58 +0200

    my portfolio

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './header.css'
+
+
+const Header = () => {
+  return (
+    <Header>
+      <div className="container header_container">
+        <h5>Hey ! I'm</h5>
+        <h1>UWIRINGIYIMANA  Marthe</h1> 
+        <h5 className="text-light">Fullstack Developer</h5>  
+      
+     </div>  
+    </Header>
+  )
+  }
+
+export default Header
